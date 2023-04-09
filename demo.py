6c48684fa43dc082d775cbe4c88407052d814afe
jagadeeshreddyr <jagadeeshreddy1997@gmail.com>
2023-04-09 23:36:31 +0530

    kniter widget

--- demo.py
+++ demo.py
@@ -24,15 +24,13 @@ for count,row in data.iterrows():
 #     rate = fuel 
 
 # print(dicts)
-def callback(selection):
+def callback():
     print(data.loc[data['city']==selection.widget.get(),'Petrol Price'].values[0].split('₹')[0])
 
 
 def on_change():
     try:
         print(float(name.widget.get()))
-        print(float(name2.widget.get()))
-        print(name1.wi)
     except:
         messagebox.showwarning("showwarning", "Please Enter number")
 
@@ -50,6 +48,11 @@ def on_change2(e2):
 
 def main():
 
+    
+    
+    
+
+
 
     root = tk.Tk()
     root.geometry('500x300')
@@ -62,6 +65,7 @@ def main():
 
 
     var_material = tk.StringVar()
+    user_input = tk.StringVar(root)
 
     combo_material = ttk.Combobox(root, values=list(dicts.keys()), justify="center", textvariable=var_material,width=20)
     combo_material.bind('<<ComboboxSelected>>', lambda event: label_selected.config(text=dicts[var_material.get()]))
@@ -71,21 +75,24 @@ def main():
     label_selected = tk.Label(root, text="Not Selected")
     label_selected.grid(row=1, column=2)
     combo_material.current(0)
-    tk.Label(root, text='Enter Mileage of your Bike/Car ',  bd=6).grid(row=3, column=0,pady=(3,3))
-    spacer1 = tk.Label(root, text="")
-    spacer1.grid(row=3, column=1)
+    ok = tk.Label(root, text='Enter Mileage of your Bike/Car ',  bd=5).grid(row=3, column=0,pady=(3,3))
+
+    # spacer1 = tk.Label(root, text="")
+    # spacer1.grid(row=3, column=1)
     #name1 = tk.Label(root, text = '', font=('calibre',10, 'bold')).grid(row=3, column = 0)
     global name,name2
     name = tk.Entry(root,text=' ',width=25)
     name.grid(row=3, column = 2,sticky=W)
     name.bind("<Return>", on_change)
     tk.Label(root, text='(km/l)',bd=0.5).grid(row=3,column=3)
+    # ttk.Button(root, text= 'ok',   command=verify).grid(row=3 , column=4)
 
-    tk.Label(root, text='Total number of km to be Travelled:',  bd=6).grid(row=4, column=0,pady=(3,3))
-    name2 = tk.Entry(root,text=' ',width=25)
-    name2.grid(row=4, column = 2,sticky=W)
-    name2.bind("<Return>", on_change2)
+    # tk.Label(root, text='Total number of km to be Travelled:',  bd=6).grid(row=4, column=0,pady=(3,3))
+    # name2 = tk.Entry(root,text=' ',width=25)
+    # name2.grid(row=4, column = 2,sticky=W)
+    # name2.bind("<Return>", on_change2)
     root.mainloop()
+   
 
 
 
